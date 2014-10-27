cd83ca9a074d39e9a01978bf6bdac792874051e4
Sean Clayton <clayton.sean@gmail.com>
2014-10-27 14:28:59 -0400

    added more initial mocha support

--- test/test.js
+++ test/test.js
@@ -1,3 +1,13 @@
 // I don't know what I'm doing yet.
 // Still learning tests!
 // I will, though, implement Mocha shortly :3
+
+var assert = require("assert")
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
